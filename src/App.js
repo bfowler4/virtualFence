6369cb951981cd79290ac29c58d063731db70f52
Levi Porter <levi@levibrooke.com>
2018-03-10 09:55:58 -1000

    Setup boilerplate google map

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+import { GoogleApiWrapper } from 'google-maps-react';
+import MapContainer from './MapContainer';
 
 class App extends Component {
   render() {
+    console.log('this.props', this.props);
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <MapContainer google={this.props.google} />
       </div>
     );
   }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  version: '3.31',
+  apiKey: 'AIzaSyDMg1_-uK-krL0Yt01gtjrjkSHamT6yQzY'
+})(App);
---
+++ src/MapContainer.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+// import GoogleMapsLoader from 'google-maps';
+
+// GoogleMapsLoader.VERSION = '3.31';
+
+export default class MapContainer extends Component {
+
+  componentDidUpdate() {
+    this.loadMap();
+  }
+
+  loadMap() {
+    if (this.props && this.props.google) {
+      const { google } = this.props;
+      const maps = google.maps;
+
+      const mapRef = this.refs.map;
+      const node = ReactDOM.findDOMNode(mapRef);
+
+      const mapConfig = Object.assign({}, {
+        center: {lat: 20.223929, lng: -155.796822},
+        zoom: 18,
+        mapTypeId: 'hybrid',
+      });
+
+      this.map = new maps.Map(node, mapConfig);
+    }
+  }
+
+  render() {
+    const style = {
+      width: '80vw',
+      height: '50vh'
+    };
+
+    return (
+      <div ref="map" style={style}>
+        loading map...
+      </div>
+    )
+  }
+}
